97da2e4f56a9378557ac8c715095338a93834704
YasserAO <aouffenyasser@gmail.com>
2025-04-30 00:05:00 +0100

    Home

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
-export default function Page() {
+import { currentUser } from "@clerk/nextjs/server";
+import { cookies } from "next/headers";
+
+export default async function Page() {
+  const user = await currentUser();
+  const cookie = await cookies();
+  const corpIndex = cookie.get("CorpSelection")?.value;
   return (
-    <div className="px-[10%] py-10">
-      <h1>Welcome</h1>
+    <div className="px-[10%] py-10 flex-1">
+      <h1>Welcome {user && <span>{user.fullName}</span>}</h1>
+      {corpIndex && (
+        <h2>
+          You are Selecting corpIndex: <span>{corpIndex}</span>{" "}
+        </h2>
+      )}
 
       <h1>Page Content</h1>
       <p>
