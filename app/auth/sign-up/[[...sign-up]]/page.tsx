ab1f2f00921131b9f85fd7d5e61e0040d335d3e4
YasserAO <aouffenyasser@gmail.com>
2025-04-29 13:03:07 +0100

    fix signin signup centering

--- app/auth/sign-up/[[...sign-up]]/page.tsx
+++ app/auth/sign-up/[[...sign-up]]/page.tsx
@@ -1,7 +1,7 @@
 import { SignUp } from "@clerk/nextjs";
 const Page = () => {
   return (
-    <div className="flex justify-center flex-1 items-center flex-col h-screen">
+    <div className="flex justify-center flex-1 items-center flex-col">
       <SignUp></SignUp>
     </div>
   );
