feb505f028a269b5824546edaa1c9671be983411
YasserAO <aouffenyasser@gmail.com>
2025-04-29 13:37:30 +0100

    clerk theme sync

--- app/auth/sign-in/[[...sign-in]]/page.tsx
+++ app/auth/sign-in/[[...sign-in]]/page.tsx
@@ -1,8 +1,17 @@
+"use client";
+import { dark } from "@clerk/themes";
+import { useTheme } from "next-themes";
 import { SignIn } from "@clerk/nextjs";
 const Page = () => {
+  const { resolvedTheme } = useTheme();
   return (
     <div className="flex justify-center flex-1 items-center flex-col">
-      <SignIn></SignIn>
+      <SignIn
+        appearance={{
+          baseTheme: resolvedTheme === "dark" ? dark : undefined,
+        }}
+        // path="/auth/sign-in"
+      ></SignIn>
     </div>
   );
 };
--- app/auth/sign-up/[[...sign-up]]/page.tsx
+++ app/auth/sign-up/[[...sign-up]]/page.tsx
@@ -1,8 +1,16 @@
+"use client";
+import { dark } from "@clerk/themes";
+import { useTheme } from "next-themes";
 import { SignUp } from "@clerk/nextjs";
 const Page = () => {
+  const { resolvedTheme } = useTheme();
   return (
     <div className="flex justify-center flex-1 items-center flex-col">
-      <SignUp></SignUp>
+      <SignUp
+        appearance={{
+          baseTheme: resolvedTheme === "dark" ? dark : undefined,
+        }}
+      ></SignUp>
     </div>
   );
 };
