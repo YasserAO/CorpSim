a35390b83ee4d6dc62496b2b44d65b222dd1274f
YasserAO <aouffenyasser@gmail.com>
2025-04-29 13:01:58 +0100

    theme provider added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,11 @@
+import { ThemeProvider } from "next-themes";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import AppSideBar from "@/components/AppSideBar";
-import {
-  ClerkProvider,
-  SignInButton,
-  SignUpButton,
-  SignedIn,
-  SignedOut,
-  SignIn,
-  UserButton,
-} from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 import { cookies } from "next/headers";
-
+import { NavMain } from "@/components/navMenu";
+import { UniversalWrapper } from "@/components/universalWrapper";
 export default async function RootLayout({
   children,
 }: Readonly<{
@@ -21,18 +15,29 @@ export default async function RootLayout({
   const side_toggle =
     cookie.get("sidebar_state")?.value == "true" ? true : false;
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={` antialiased flex-1`}>
-        <ClerkProvider>
-          <header className="flex items-center h-fit justify-between md:px-[10%] lg-px[30%]"></header>
-          <SidebarProvider defaultOpen={side_toggle}>
-            <AppSideBar></AppSideBar>
-            <main>
-              <SidebarTrigger></SidebarTrigger>
-              {children}
-            </main>
-          </SidebarProvider>
-        </ClerkProvider>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <ClerkProvider>
+            <header className="flex items-center h-fit justify-between md:px-[10%] lg-px[30%]"></header>
+
+            <SidebarProvider defaultOpen={side_toggle}>
+              <UniversalWrapper>
+                <AppSideBar></AppSideBar>
+              </UniversalWrapper>
+              <main className="w-full pt-2 flex flex-col flex-1">
+                <NavMain></NavMain>
+
+                <div className="flex-1 flex flex-col">{children}</div>
+              </main>
+            </SidebarProvider>
+          </ClerkProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
